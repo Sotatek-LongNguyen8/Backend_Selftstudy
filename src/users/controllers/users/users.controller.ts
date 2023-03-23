9222c2bd82b1c21581ff4a0acc8a16a2a094f6e2
KimLong <longckek@gmail.com>
2023-03-23 18:00:54 +0700

    tutorial 7 - 10

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,5 +1,24 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseFilters,
+  UseInterceptors,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { UserNotFoundException } from 'src/users/exceptions/UserNotFound.exception';
+import { HttpExceptionFilter } from 'src/users/filters/HttpException.filter';
 import { UsersService } from 'src/users/services/users/users.service';
+import { SerializedUser } from 'src/users/types';
 
 @Controller('users')
 export class UsersController {
@@ -8,7 +27,33 @@ export class UsersController {
   ) {}
 
   @Get('')
+  @UseInterceptors(ClassSerializerInterceptor)
   getUsers() {
     return this.userService.getUsers();
+  }
+
+  @Get('/username/:username')
+  @UseInterceptors(ClassSerializerInterceptor)
+  getByUsername(@Param('username') username: string) {
+    const user = this.userService.getUserByUsername(username);
+    if (user) return new SerializedUser(user);
+    else throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+  }
+
+  @Get('id/:id')
+  @UseInterceptors(ClassSerializerInterceptor)
+  @UseFilters(HttpExceptionFilter)
+  getById(@Param('id', ParseIntPipe) id: number) {
+    const user = this.userService.getUserById(id);
+    if (user) return new SerializedUser(user);
+    else {
+      throw new UserNotFoundException();
+    }
+  }
+
+  @Post('create')
+  @UsePipes(ValidationPipe)
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
   }
 }
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,32 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Injectable,
+  UseInterceptors,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { plainToClass } from 'class-transformer';
+import { User as UserEntity } from 'src/typeorm';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 import { SerializedUser, User } from 'src/users/types';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  private users: User[] = [
-    {
-      username: 'long',
-      password: 'long',
-    },
-    {
-      username: 'long1',
-      password: 'long1',
-    },
-    {
-      username: 'long2',
-      password: 'long2',
-    },
-    {
-      username: 'long3',
-      password: 'long3',
-    },
-  ];
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  private users: User[] = [];
+
   getUsers() {
-    return this.users.map((user) => plainToClass(SerializedUser, user));
+    return this.users.map((user) => new SerializedUser(user));
   }
 
   getUserByUsername(username: string) {
     return this.users.find((user) => user.username === username);
+  }
+
+  getUserById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  createUser(createUserDto: CreateUserDto) {
+    const newUser = this.userRepository.create(createUserDto);
+    return this.userRepository.save(newUser);
   }
 }
